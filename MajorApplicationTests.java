a270a67bf27bb09b2fbcd6f2c493aef64bde1610
MD Tousif Rahman <115324615+TRtousiff@users.noreply.github.com>
2024-03-08 23:08:03 +0600

    Add files via upload

---
+++ MajorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group5.major;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MajorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
